bc7cd4259dff10ca200f1631077fb571a9a4b2c4
aotrek <otatbpdevelopment@gmail.com>
2023-07-30 11:50:50 +0600

    [ADD] - added with a active classlist

--- resourse/js/script.js
+++ resourse/js/script.js
@@ -1,14 +1,16 @@
 let section = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
-window.onscroll = () => {
+// console.log(navLinks)
+
+/* window.onscroll = () => {
     section.forEach(sec => {
         let top = window.screenY;
         let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-        if (top => offset && top < offset + height)
+        if (top >= offset && top < offset + height)
         {
             navLinks.forEach(links => {
                 links.classList.remove('active');
@@ -16,7 +18,7 @@ window.onscroll = () => {
             });
         };
     });
-};
+}; */
 
 
 // ========================= sticky menu navbar ======================
@@ -24,3 +26,22 @@ window.onscroll = () => {
 
 let header = document.querySelector('header');
 header.classList.toggle('sticky',window.scrollY > 100);
+
+
+
+
+// active call with a header section
+
+const nav = document.querySelectorAll(".navAc");
+// console.log(nav)
+
+nav.forEach((item)=>{
+    // console.log(item)
+    item.addEventListener("click",function(e){
+        const current = document.getElementsByClassName("active");
+        // console.log(current[0].className)
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+        
+    })
+})
